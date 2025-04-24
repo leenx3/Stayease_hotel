f3e6b4e556059efe8493695bbefa26677b080704
JosefZ13 <110061035+JosefZ13@users.noreply.github.com>
2025-04-24 21:40:54 +0200

    Update urls.py

--- StayEase/urls.py
+++ StayEase/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('booking.urls')),  # Include URLs from the booking app
+    path('', include('booking.urls')),  
 ]
